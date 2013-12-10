640bc628b150007ed8ba8f9acef0f7d3f3dd1d27
Christopher Patton <chpatton013@gmail.com>
2013-12-09 20:21:13 -0800

    Basic config

---
+++ configure/parameters.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+###############################################################################
+# Important parameters.
+###############################################################################
+
+# Physical block device to install system to.
+disk='/dev/sdx'
+# Passphrase for root luks container.
+luks_pass=''
+# Password for root user.
+root_pass=''
+
+###############################################################################
+# Not-so-important parameters.
+###############################################################################
+
+# System configuration.
+lvm_volume='system'
+locale="en_US.utf8"
+hostname="arch-box"
+timezone="America/Los_Angeles"
+# Ramdisk configuration.
+modules='virtio virtio_blk virtio_pci virtio_net'
+
